df18fffe157c77bcc1bdcb9e6ef0733d83215953
mkfreeberg <mkfreeberg@hotmail.com>
2013-07-07 07:38:28 -0700

    Enhancements for portability by using boost
    1. Used iterators
    2. Used boost library to guarantee integer width
    3. Made a call to boost::algorithm::tolower to guarantee lowercase so the math would work
    4. Since 1.1 seems to expect it (although that's debatable), I provided a fourth algorithm that uses std::set
    
    Note that if you submit a string that includes a case-transformed version of a prior character -- example, "AbCdEfGa", the third routine will return "not unique" while the others will return "unique". That should be acceptable in the code interview if you say so, since 1.1 doesn't specify.

--- 1.1.cpp
+++ 1.1.cpp
@@ -2,52 +2,66 @@
 #include <cstring>
 using namespace std;
 
-bool isUnique1(string s)
-{
+bool isUnique1(string s) {
     bool a[256];
     memset(a, 0, sizeof(a));
-    int len = s.length();
-    for(int i=0; i<len; ++i)
-    {
-        int v = (int)s[i];
-        if(a[v]) return false;
-        a[v] = true;
+    bool not_unique = false;
+    for(string::iterator iter = s.begin(); !not_unique && iter != s.end(); ++iter) {
+        int v = (int) *iter;
+        if (a[v]) not_unique = true; else a[v] = true;
     }
-    return true;
+    return !not_unique;
 }
 
-bool isUnique2(string s)
-{
-    int a[8];
+#include <boost/cstdint.hpp>
+
+bool isUnique2(string s) {
+    boost::uint32_t a[8];
     memset(a, 0, sizeof(a));
-    int len = s.length();
-    for(int i=0; i<len; ++i)
-    {
-        int v = (int)s[i];
+    bool not_unique = false;
+    for(string::iterator iter = s.begin(); !not_unique && iter != s.end(); ++iter) {
+        int v = (int) *iter;
         int idx = v/32, shift=v%32;
-        if(a[idx] & (1<<shift)) return false;
-        a[idx] |= (1<<shift);
+        if(a[idx] & (1<<shift)) not_unique = true; else a[idx] |= (1<<shift);
     }
-    return true;
+    return !not_unique;
 }
 
-bool isUnique3(string s)
-{
-    int check = 0;
-    int len = s.length();
-    for(int i=0; i<len; ++i)
-    {
-        int v = (int)(s[i]-'a');
-        if(check & (1<<v)) return false;
-        check |= (1<<v);
+#include <boost/algorithm/string.hpp>
+
+bool isUnique3(string s) {      /* You were assuming this was lowercase so I made this case-insensitive */
+    boost::uint32_t check = 0;  /* Exercise 1.1 does not specify whether or not that is the requirement */
+    bool not_unique = false;
+    for(string::iterator iter = s.begin(); !not_unique && iter != s.end(); ++iter) {
+        int v = boost::algorithm::to_lower( *iter ) - 'a';
+        if(check & (1<<v)) not_unique = true; else check |= (1<<v);
     }
-    return true;
+    return !not_unique;
 }
-int main()
-{
-    string s1 = "i am hawstein.";
-    string s2 = "abcdefghijklmnopqrstuvwxyz1234567890";
-    cout<<isUnique1(s1)<<" "<<isUnique1(s2)<<endl;
-    cout<<isUnique2(s1)<<" "<<isUnique2(s2)<<endl;
+
+#include <set>
+
+bool isUnique4(string String) { /* Exercise 1.1 says "What if you cannot use data structures? From this I'm */
+                                /* inferring that the interviewer would want to see both implementations    */
+                                /* This one uses a data structure of std::set, which presumably works in    */
+                                /* O(log n) time */
+    set<char> Set;
+    bool not_unique = false;
+    for(string::iterator iter = String.begin(); !not_unique && iter != String.end(); ++iter) {
+        if ( Set.find( *iter ) == Set.end() ) Set.add( *iter ); else not_unique = true;
+    }
+    return !not_unique;
+}
+
+int main( int argc, char *argv[] ) {
+    if ( argc < 2 ) {
+        cout << "Syntax: unique_1_1 {string}" << endl;
+    } else {
+        string TheString( argv[1] );
+        cout << "1: \"" << TheString << "\" is " << (isUnique1(TheString)?"unique":"not unique") << "." << endl;
+        cout << "2: \"" << TheString << "\" is " << (isUnique2(TheString)?"unique":"not unique") << "." << endl;
+        cout << "3: \"" << TheString << "\" is " << (isUnique3(TheString)?"unique":"not unique") << "." << endl;
+        cout << "4: \"" << TheString << "\" is " << (isUnique4(TheString)?"unique":"not unique") << "." << endl;
+    }
     return 0;
 }
